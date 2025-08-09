0ec7b4f7f46163ca9defb4c8d4d454b73288dc10
Saeed beraia <saeed.beraia1996@gmail.com>
2025-08-09 09:02:39 +0300

    profile page

--- app/[locale]/(pages)/profile/page.tsx
+++ app/[locale]/(pages)/profile/page.tsx
@@ -1,3 +1,41 @@
-export default function ProfilePage() {
-  return <div>Profile Page</div>;
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { getTranslations } from "next-intl/server";
+
+export default async function ProfilePage() {
+  const t = await getTranslations("common");
+  const user = {
+    userName: "saeed_beraia",
+    firstName: "Saeed",
+    lastName: "Beraia",
+    email: "saeed@example.com",
+    phone: "+966 555 123 456",
+    avatar: undefined,
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-6 max-w-md mx-auto mt-8 p-6 rounded-lg border text-card-foreground shadow-sm">
+      <Avatar className="h-24 w-24">
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
+      <div className="flex flex-col items-center gap-1">
+        <h3 className="text-sm md:text-2xl font-semibold text-center">
+          {user.firstName} {user.lastName} ({user.userName})
+        </h3>
+        <p className="text-sm md:text-base text-muted-foreground">
+          {user.email}
+        </p>
+        <p className="text-sm md:text-base text-muted-foreground">
+          {user.phone}
+        </p>
+      </div>
+
+      <div className="flex gap-2">
+        <Button>{t("edit")}</Button>
+        <Button variant="outline">{t("changePassword")}</Button>
+      </div>
+    </div>
+  );
 }
