b6de8964cecf2f72fd9715168c3d3645ba05c675
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2025-08-13 00:10:19 +0300

    intagrat subscriptionPage

--- app/[locale]/(pages)/subscription/page.tsx
+++ app/[locale]/(pages)/subscription/page.tsx
@@ -1,11 +1,49 @@
+"use client";
+import { useState, useEffect } from "react";
 import { SubscriptionsFilters } from "./components/SubscriptionsFilters";
-import { SubscriptionsList } from "./components/SubscriptionsList";
+import { useSubscriptionsSearch } from "@/orval/subscriptions/subscriptions";
+import { SkeletonSubCard } from "./components/SkeletonSubCard";
+import { SubsecriptionCard } from "./components/SubsecriptionCard";
 
 export default function SubscriptionPage() {
+  
+  const [filters, setFilters] = useState({
+    search: "",
+    subscriptionBundleId: "",
+    status: "",
+    // deviceStatus: "",
+  });
+
+  const subscriptionsSearch = useSubscriptionsSearch();
+  const subscriptionsDate = subscriptionsSearch?.data?.data;
+  // تشغيل البحث تلقائيًا عند تغيير الفلاتر
+  useEffect(() => {
+    subscriptionsSearch.mutate({
+      data: {
+        search: filters.search !== "" ? filters.search : null,
+        subscriptionBundleId: filters.subscriptionBundleId !== "" ? Number(filters.subscriptionBundleId) : null,
+        status: filters.status !== "" ? Number(filters.status) : null,
+        // deviceStatus: filters.deviceStatus,
+      },
+    });
+  }, [filters]);
+ 
   return (
     <>
-      <SubscriptionsFilters />
-      <SubscriptionsList />
-      </>
-  )
+      <SubscriptionsFilters filters={filters} setFilters={setFilters} />
+
+      {
+        subscriptionsSearch.isPending ? (
+          <div className="space-y-4">
+            {Array.from({ length: 3 }).map((_, i) => (
+              <SkeletonSubCard key={i} />
+            ))}
+          </div>
+        ) : (
+          <SubsecriptionCard subscriptions={subscriptionsDate || []}/>
+        )
+      }
+    </>
+  );
 }
+
